fc3077c89a5968d24d70a6e3b46e07331835a391
Gaurav Muskan <muskangaurav017@gmail.com>
2026-07-05 11:05:55 +0530

    sort path is integrated with Query Params

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
+import tkinter as tk
+from tkinter import messagebox
 
 app = FastAPI()
 
@@ -24,10 +26,26 @@ def view():
     return data
 
 @app.get("/patient/{patient_id}")
-def patient_data(patient_id: str = Path(..., description='ID of the patient in the DB.', example='P001')):
+def patient_data(patient_id: str = Path(..., description='ID of the patient in the DB.', examples='P001')):
     # load all Patient data
     data = load_data()
 
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail='Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), 
+                  order: str = Query('asc', description='sort in desc or asc order')):
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail='Invalid fields, select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between asc or desc')
+    
+    data = load_data()
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by, 0), reverse=sort_order)
+    return sorted_data
