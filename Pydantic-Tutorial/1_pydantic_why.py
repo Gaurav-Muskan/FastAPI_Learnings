0e8201020982bdafce9c31a748acfc40bface384
Gaurav Muskan <muskangaurav017@gmail.com>
2026-07-05 15:41:03 +0530

    Optional and default feature is added.

--- Pydantic-Tutorial/1_pydantic_why.py
+++ Pydantic-Tutorial/1_pydantic_why.py
@@ -1,14 +1,23 @@
 from pydantic import BaseModel
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
 
      name: str
      age: int
+     weight: float
+    #  default value for married is added.
+     married: bool = False
+     allergies: Optional[List[str]] = None
+     contact_details: Dict[str, str]
 
 
 def insert_patient_data(patient: Patient):
     print(patient.name)
     print(patient.age)
+    # default values will be shown for married
+    print(patient.married)
+    print(patient.allergies)
     print('inserted into database')
     
 def update_patient_data(patient: Patient):
@@ -16,8 +25,7 @@ def update_patient_data(patient: Patient):
     print(patient.age)
     print('Updated in database')
 
-patient_info = {'name': 'Visha', 'age': 25}
+patient_info = {'name': 'Visha', 'age': 25, 'weight': 75.2, 'contact_details': {'email': 'gaurav@gmail.com', 'phone': '9876543210'}}
 patient1 = Patient(**patient_info)
 
-# insert_patient_data(patient1)
-update_patient_data(patient1)
+insert_patient_data(patient1)
