a3c5adfde95812d2565cbec9af997b25737649c1
Gaurav Muskan <muskangaurav017@gmail.com>
2026-07-05 11:15:10 +0530

    Add argument(invalidated) for insert_patient_data fn.

---
+++ Pydantic-Tutorial/1_pydantic_why.py
@@ -0,0 +1,11 @@
+
+
+def insert_patient_data(name, age):
+
+    print(name)
+    print(age)
+    print('inserted into database')
+
+
+# No validation is there for name and age.
+insert_patient_data('nitish', 'thirty')
