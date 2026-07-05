b30399a4e0fc35a7f890c11341f9d38f138b4344
Gaurav Muskan <muskangaurav017@gmail.com>
2026-07-05 11:27:54 +0530

    Type Error is added.

--- Pydantic-Tutorial/1_pydantic_why.py
+++ Pydantic-Tutorial/1_pydantic_why.py
@@ -1,11 +1,12 @@
 
 
 def insert_patient_data(name: str, age: int):
+    if type(name) == str and type(age) == int:
+        print(name)
+        print(age)
+        print('inserted into database')
+    else:
+        raise TypeError('Incorrect data type.')
 
-    print(name)
-    print(age)
-    print('inserted into database')
-
-
-# Type Hints are added for the parameters.
-insert_patient_data("Gaurav", 25)
+# Type Error is added for the parameters.
+insert_patient_data("Gaurav", '25') 
