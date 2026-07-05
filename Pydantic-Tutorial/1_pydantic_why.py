e2de9cd83adb24481e93abee2ca9a97e44d640f6
Gaurav Muskan <muskangaurav017@gmail.com>
2026-07-05 16:06:24 +0530

    Add Pydantic Field examples for custom fields, metadata, default values, and strict type checking

--- Pydantic-Tutorial/1_pydantic_why.py
+++ Pydantic-Tutorial/1_pydantic_why.py
@@ -1,14 +1,16 @@
-from pydantic import BaseModel
-from typing import List, Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
 
-     name: str
+     name: Annotated[str, Field(max_length=50, title='Name of the patient', description='Give the name of the patient in less than 50 Characters.', examples=["Solu", "Anit"])]
      age: int
-     weight: float
+     email: EmailStr
+     linkedin_url: AnyUrl
+     weight: Annotated[float, Field(gt=0, strict=True)]
     #  default value for married is added.
-     married: bool = False
-     allergies: Optional[List[str]] = None
+     married: Annotated[bool, Field(default=False, description='Is the patient married or not.')]
+     allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
      contact_details: Dict[str, str]
 
 
@@ -25,7 +27,7 @@ def update_patient_data(patient: Patient):
     print(patient.age)
     print('Updated in database')
 
-patient_info = {'name': 'Visha', 'age': 25, 'weight': 75.2, 'contact_details': {'email': 'gaurav@gmail.com', 'phone': '9876543210'}}
+patient_info = {'name': 'Visha', 'age': 25, 'email': 'gaurav@gmail.com', 'linkedin_url': 'http://linkedin.com/visha19', 'weight': '75.2', 'contact_details': {'email': 'gaurav@gmail.com', 'phone': '9876543210'}}
 patient1 = Patient(**patient_info)
 
 insert_patient_data(patient1)
